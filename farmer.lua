9d06b095439d6deff68a90dd4a5967f37b6343fb
saji <9110284+kschamplin@users.noreply.github.com>
2022-03-07 23:15:19 -0600

    added multirow farming

--- farmer.lua
+++ farmer.lua
@@ -51,48 +51,57 @@ end
 -- deposit wood, apples, sticks.
 -- refuel if necessary (check fuel)
 while true do
-nav.moveAbs({ z = 5, x = 1, y = 0})
-
-nav.setDirection(2)
--- check if tree
-
-while true do
-res, detail = turtle.inspect()
-if (res) then
-	if (detail.name == "minecraft:birch_log") then
-		-- felling routine
-		print("chop chop")
-		fellTree()
+	nav.moveAbs({ z = 5, x = 0, y = 0})
+	
+	nav.moveAbs({x = 1})
+	nav.setDirection(2)
+	
+	while true do
+		-- do we have tree?
+		res, detail = turtle.inspect()
+		if (res) then
+			if (detail.name == "minecraft:birch_log") then
+				-- felling routine
+				print("chop chop")
+				fellTree()
+			end
+		end
+		
+		-- goto next tree
+		result = nav.moveRel({right = -2})
+		if (not result) then
+			print("hit end of row")
+			-- try next row
+			result = nav.moveRel({forward = -3})
+			if (not result) then
+				print("hit end of farm, collecting")
+				break
+			end
+			-- else we go back to x = 1
+			nav.moveAbs({x = 1})
+			nav.setDirection(2)
+		end
 	end
-end
-
--- goto next tree
-result = nav.moveRel({right = -2})
-if (not result) then
-	print("hit something")
-	break
-end
-end
-
--- now go to collection point
-nav.moveAbs({x = 6, y = -2, z = 0})
-
-for i = 1,30 do
-	os.sleep(10)
-	turtle.suckDown()
-end
-nav.moveRel({up = 2})
-nav.goHome()
-
--- deposit
-nav.setDirection(0)
-turtle.select(1)
--- refuel
-if (turtle.getFuelLevel() < 600) then
-	turtle.refuel(10)
-end
-
-turtle.drop(turtle.getItemCount())
-os.sleep(15 * 60)
+		
+	-- now go to collection point
+	nav.moveAbs({x = 6, y = -2, z = 0})
+	
+	for i = 1,30 do
+		os.sleep(10)
+		turtle.suckDown()
+	end
+	nav.moveRel({up = 2})
+	nav.goHome()
+	
+	-- deposit
+	nav.setDirection(0)
+	turtle.select(1)
+	-- refuel
+	if (turtle.getFuelLevel() < 600) then
+		turtle.refuel(10)
+	end
+	
+	turtle.drop(turtle.getItemCount())
+	os.sleep(5 * 60)
 end
 print("end of program")
